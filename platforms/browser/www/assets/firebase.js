394f1a641c7e100c652fff872b6dd74a8731e7bb
FoxPawPaws <palmermichael@rogers.com>
2019-01-20 17:34:18 -0500

    added config.xml

---
+++ platforms/browser/www/assets/firebase.js
@@ -0,0 +1,12 @@
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCf60Xj6daTPh_RJHUR1syFYsi_xKD7-AM",
+    authDomain: "highjump-928e1.firebaseapp.com",
+    databaseURL: "https://highjump-928e1.firebaseio.com",
+    projectId: "highjump-928e1",
+    storageBucket: "highjump-928e1.appspot.com",
+    messagingSenderId: "459483522784"
+  };
+  firebase.initializeApp(config);
+</script>
